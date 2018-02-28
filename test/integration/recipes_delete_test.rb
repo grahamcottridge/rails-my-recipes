e284021993e0c9b20122cddb5f254511cef3a4de
Graham Cottridge <codinggc@gmail.com>
2018-02-28 22:05:56 +0000

    Implemented delete recipe functionality to controller

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(name: "graham", email: "graham@example.com")
+    @recipe = Recipe.create(name: "Pasta", description: "bolognese",  chef: @chef)
+  end
+
+  test "successfully delete a recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select "a[href=?]", recipe_path(@recipe), text: "Delete recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
+  end
+
+
+end
