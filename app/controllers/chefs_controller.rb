c886860ad38c9d0fca00d5f8715aa45ba5c52091
Graham Cottridge <codinggc@gmail.com>
2018-04-10 19:52:19 +0100

    Implemented chef signup with tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,4 +4,24 @@ class ChefsController < ApplicationController
     @chef = Chef.new
   end
 
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "Welcome #{@chef.name} to My Recipes"
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -6,4 +6,28 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "reject an invalid signup" do
+    get signup_path
+    assert_no_difference "Chef.count" do
+      post chefs_path, params: { chef: { name: " ",
+                                 email: " ", password: "password",
+                                    password_confirmation: " " } }
+    end
+    assert_template 'chefs/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test "accept valid signup" do
+    get signup_path
+    assert_difference "Chef.count", 1 do
+      post chefs_path, params: { chef: { name: "graham",
+                email: "graham@example.com", password: "password",
+                              password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'chefs/show'
+    assert_not flash.empty?
+  end
+
 end
