c137640228152d0afe202501e4be3836ab500cea
Graham Cottridge <codinggc@gmail.com>
2018-04-11 18:00:26 +0100

    Implement delete chef with associated recipes functionality

--- test/integration/chefs_index_test.rb
+++ test/integration/chefs_index_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ChefsIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @chef = Chef.create!(name: "graham", email: "graham@example.com",
+                     password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(name: "john", email: "john@example.com",
+                     password: "password", password_confirmation: "password")
+  end
+
+  test "should get chefs listing" do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_select "a[href=?]", chef_path(@chef), text: @chef.name.capitalize
+    assert_select "a[href=?]", chef_path(@chef2), text: @chef2.name.capitalize
+  end
+
+  test "should delete chef" do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef2)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end
+
 end
