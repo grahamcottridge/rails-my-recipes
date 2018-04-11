cf4410d74fa164efa8b0128322b7fe39e378f290
Graham Cottridge <codinggc@gmail.com>
2018-04-11 22:22:46 +0100

    Completed navigation layout with tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -23,6 +23,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
   end
 
   test "should get recipes show" do
+    sign_in_as(@chef, "password")
     get recipe_path(@recipe)
     assert_template 'recipes/show'
     assert_match @recipe.name, response.body
@@ -34,17 +35,18 @@ class RecipesTest < ActionDispatch::IntegrationTest
   end
 
   test "create new valid recipe" do
+    sign_in_as(@chef, "password")
     get new_recipe_path
     assert_template 'recipes/new'
-  #   name_of_recipe = "chicken saute"
-  #   description_of_recipe = "add chicken, add vegetables,
-  #                    cook for 20 minutes, serve delicious meal"
-  #   assert_difference 'Recipe.count', 1 do
-  #   post recipes_path, params: { recipe: { name: name_of_recipe,
-  #                     description: description_of_recipe}}
-  # end
-    # follow_redirect!
-    # assert_match name_of_recipe.capitalize, response.body
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicken, add vegetables,
+                     cook for 20 minutes, serve delicious meal"
+    assert_difference 'Recipe.count', 1 do
+    post recipes_path, params: { recipe: { name: name_of_recipe,
+                      description: description_of_recipe}}
+  end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
     # assert_match description_of_recipe, response.body
   end
 
